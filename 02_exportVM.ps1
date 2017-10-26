7d6fa94e1097e8b382f74aa1ceb95d5f3fc37b66
Daniel <danieloliversoftware@gmail.com>
2017-10-25 22:54:19 -0400

    clone VM

---
+++ 02_exportVM.ps1
@@ -0,0 +1,19 @@
+Param(
+    [Parameter(Mandatory=$true,
+             HelpMessage="The Name of the Snapshot exported")]
+    [string]$snapName,
+
+    [Parameter(Mandatory=$true,
+            HelpMessage="The Name of the VM")]
+    [string]$vmName
+)
+$exportPath = "C:\Virtual Machines\$snapName"
+
+$vm = Get-VM -Name $vmName
+$snapshot = Export-VMSnapshot -VM $vm -Name $snapName $exportPath -Passthru
+
+New-VM -Name $snapshot.Name -NoVHD -MemoryStartupBytes $snapshot.MemoryStartup -switch "centos"
+
+$hardDrive = Get-ChildItem -LiteralPath "C:\Virtual Machines\snap\admin\Virtual Hard Disks\" | Where-Object { $_.Name -like "*.vhdx" }
+
+Add-VMHardDiskDrive -Path $hardDrive.FullName -VMName $snapName
--- 99_trashVM.ps1
+++ 99_trashVM.ps1
@@ -7,5 +7,5 @@ Param(
 
 Stop-VM -Name $vmName -TurnOff
 Remove-VM -Name $vmName -Force
-Remove-Item -Path "C:\Virtual Machines\$vmName\$vmName.vhdx"
+Remove-Item -Path "C:\Virtual Machines\$vmName" -Force
 
